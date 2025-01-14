fb0a4ca6a15143b0d1922865f99bb2c63264fc84
Tatsuru Hisanaga <tatsu72x@gmail.com>
2025-01-14 20:41:39 +0900

    refactor: フォーマットを適用

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,32 @@
-import { useState, useEffect } from 'react'
-import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { supabase } from '../lib/supabase'
-import Auth from '../components/Auth'
-import Account from '../components/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
-import Test from '@/components/Test'
+import Test from '@/components/Test';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+import { Session } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import Auth from '../components/Auth';
+import { supabase } from '../lib/supabase';
+import React = require('react');
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   return (
-    <GluestackUIProvider
-      mode="light"><View>
-                {session && session.user ? <Test /> : <Auth />}
-                {/* {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />} */}
-              </View></GluestackUIProvider>
+    <GluestackUIProvider mode="light">
+      <View>
+        {session && session.user ? <Test /> : <Auth />}
+        {/* {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />} */}
+      </View>
+    </GluestackUIProvider>
   );
 }
